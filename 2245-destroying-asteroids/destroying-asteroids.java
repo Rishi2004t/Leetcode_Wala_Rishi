2d92d57ec40bfea13111bacd6cb6300a422956a0
Rishi Dange <manishadange779@gmail.com>
2026-05-31 08:32:11 +0530

    Time: 25 ms (93.30%) | Memory: 106.5 MB (89.15%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long Mass = mass;
+        for(int asteroid :  asteroids){
+            if(Mass <  asteroid){
+                return  false;
+
+            }
+            Mass += asteroid;
+        }
+        return true;
+    }
+}
