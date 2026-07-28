ea1fc877d710cb75f2e2e54aadf90aaa49269c2d
Rishi Dange <manishadange779@gmail.com>
2026-07-28 08:13:15 +0530

    Time: 36 ms (25.30%) | Memory: 48.4 MB (23.69%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String smallestPalindrome(String s) {
+        int[] freq =  new int[26];
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']++;
+        }
+        StringBuilder left =  new StringBuilder();
+        char middle =0;
+        for(int i = 0 ;i <  26; i++){
+            for(int j = 0; j < freq[i]/ 2; j++){
+                left.append((char)('a' + i));
+            }
+            if(freq[i] % 2 == 1){
+                middle = (char)('a' + i);
+            }
+        }
+        String right =  new StringBuilder(left).reverse().toString();
+        if(middle != 0){
+            return left.toString() +  middle +   right;
+        }
+        return left.toString() + right;
+    }
+}
